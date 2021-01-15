812a2bcd4f10223751a8999753cead7bbd91e158
Hideto1112 <ueda_hideto@hotmail.com>
2021-01-15 09:21:18 +0000

    additional_function_and_final_check

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   before_action :set_one_month, only: :show
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page]).search(params[:search])
   end
+  
 
   def show
     @worked_sum = @attendances.where.not(started_at: nil).count
